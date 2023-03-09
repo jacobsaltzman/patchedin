3e1d121940561775adb06c76f2095d0786d486ea
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 19:56:32 -0800

    Builds: full CRUD capability.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,42 @@
 class TasksController < ApplicationController
+
+  #GET /tasks
+  def index
+    @tasks = Task.all
+    render json: @tasks, status: :ok
+  end
+
+  #GET /tasks/:id
+  def show
+    task = Task.find(params[:id])
+    render json: task, status: :ok
+  end
+
+  #POST /tasks
+  def create
+    task = Task.create!(task_params)
+    render json: task, status: :created
+  end
+
+  #PATCH /tasks/:id
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    render json: task, status: :accepted
+  end
+
+  #DELETE /tasks/:id
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    head :no_content
+  end
+
+  private
+
+  def task_params
+    params.permit(:project_id, :name, :description, :completed, :difficulty)
+  end
+
+
 end
