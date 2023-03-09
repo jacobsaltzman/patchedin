14a126835ceb702de451d999de0f53ca917e969b
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 19:13:01 -0800

    Adds: show and create methods, plus strong params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  skip_before_action :authorize, only: :create
+
+  
+  def show
+    render json: @current_user, status: :ok
+  end
+
+ 
+  def create
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  private
+
+  #strong params
+  def user_params
+    params.permit(:username, :password)
+  end
+
+
 end
