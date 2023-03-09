0e88f84d13d3ac98c924cd55011bf41d85a309a3
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 17:20:44 -0800

    Builds: project boilerplate from rails g resource.

---
+++ db/migrate/20230309011449_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :title
+      t.text :description
+      t.integer :progress
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
