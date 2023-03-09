f447491a5209c0246b52ae665473bbad684bc0e5
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 19:40:03 -0800

    Builds: full CRUD capabilities.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,47 @@
 class ProjectsController < ApplicationController
+
+  #GET /projects
+  def index
+    @projects = Project.all
+    render json: projects, status: :ok
+  end
+
+
+  #GET /projects/:id
+  def show
+    project = Project.find(params[:id])
+    render json: project, status: :ok
+  end
+
+
+  #POST /projects
+  def create
+    project = Project.create!(project_params)
+    render json: project, status: :created
+  end
+
+
+  #PATCH /projects/:id
+  def update
+    project = Project.find(params[:id])
+    project.update!(project_params)
+    render json: project, status: :accepted
+  end
+
+
+  #DELETE /projects/:id
+  def destroy
+    project = Project.find(params[:id])
+    project.destroy
+    head :no_content
+  end
+
+
+
+  private
+  #strong params
+  def project_params
+    params.permit(:title, :description, :progress, :type, :user_id)
+  end
+
 end
