c030b32d25d5fea6433d6cdb4feeaf989cc0befc
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 20:13:43 -0800

    Builds: full CRUD baseline functionality.

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,41 @@
 class ContributionsController < ApplicationController
+
+  #GET /contributions
+  def index
+    @contribs = Contribution.all 
+    render json: @contribs, status: :ok
+  end
+
+  #GET /contributions/:id
+  def show
+    contrib = Contribution.find(params[:id])
+    render json: contrib, status: :ok
+  end
+
+  #POST /contributions
+  def create
+    contrib = Contribution.create!(contribution_params)
+    render json: contrib, status: :created
+  end
+
+  #PATCH /contributions/:id
+  def update
+    contrib = Contribution.find(params[:id])
+    contrib.update!(contribution_params)
+    render json: contrib, status: :accepted
+  end
+
+
+  #DELETE /contributions/:id
+  def destroy
+    contrib = Contribution.find(params[:id])
+    contrib.destroy
+    head :no_content
+  end
+
+  private
+
+  def contribution_params
+    params.permit(:user_id, :task_id, :type, :report)
+  end
 end
