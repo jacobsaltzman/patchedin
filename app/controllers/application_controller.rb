cee2d986786c18a11b59863d1f318fcefac08b97
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 18:37:02 -0800

    Adds: error handling private method and inheritance macro, and builds framework for authorization to be completed later.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::API
+  #cookie functionality on the top level
   include ActionController::Cookies
+  #catch the error and then process it with custom private method below
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
+
+
+  private
+
+  def authorize
+    #see if user exists via session
+  end
+
+  #method to render error messages from the rescue_from error catch
+  def render_unprocessable_entity(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end 
 
 
 end
