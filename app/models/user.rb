17eef166dcda81e9195ad3e42215f67beabfaaf9
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 18:03:34 -0800

    Adds: relationships, password protection, and validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  #bcrypt password macro
+  has_secure_password
+  #user can create many projects directly
+  has_many :projects
+  #user can create many contributions, which is the join table for tasks
+  has_many :contributions
+  has_many :tasks, through: :contributions
+  #validates a unique username and password existing between 4 and 15 characters
+  validates :username, uniqueness: true, presence: true
+  validates :password, length: {minimum: 4, maximum: 15}
+  
+
 end
