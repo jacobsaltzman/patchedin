5611faae33bdc5dc99442a3b65b9f6608393ff58
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 19:24:29 -0800

    Fixes: the missing ! in the create method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
  
   def create
-    user = User.create(user_params)
+    user = User.create!(user_params)
     session[:user_id] = user.id
     render json: user, status: :created
   end
