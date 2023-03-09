8aa4972442ff69ef51dd177d0bc8e89b9de125ce
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-08 18:09:02 -0800

    Adds: relationship macros and validations.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ApplicationRecord
+
+  #is created and owned by one user only
+  belongs_to :user
+  #can contain many tasks
+  has_many :tasks
+
+
+  validates :title, presence: true
+
 end
