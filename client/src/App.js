f56ed1b0fcba3817ef12bdebb814f00d3e44662f
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-12 00:02:50 -0800

    Creates: a home component and adds it along with routes to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from "react-router-dom"
 import './App.css';
+import Home from './Components/Home';
 
 function App() {
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Header</header>
+      <div className='App-Body'>
+        <Routes>
+          <Route 
+          path='/'
+          element={<Home/>}
+          />
+
+
+        </Routes>
+      </div>
+
     </div>
   );
 }
---
+++ client/src/Components/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+function Home(){
+  return (
+    <div id="home">
+      <div id="home-top-div"> 
+      <h2>Welcome</h2>
+      </div>
+
+ 
+      <div id="home-middle-div">
+        to the
+      </div>
+ 
+
+      <div id="home-bottom-div">
+      Home
+      </div>
+    </div>
+  )
+}
+
+export default Home;
