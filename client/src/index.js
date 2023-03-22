cd659fe64c7a408660c9e9cbb6efaf8d51832cd4
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-22 01:04:32 -0700

    Builds: a useContext user component to be used for global storage and setting of the current user.

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import { UserProvider } from './context/user';
 
 
 
 ReactDOM.render(
   <BrowserRouter>
+    <UserProvider>
     <App />
+    </UserProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
