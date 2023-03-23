e9dc90f68a2ce0375f6ee03244d9cfd6239480dc
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-23 12:33:12 -0700

    Adds: new wording to the home page about the site and signing in.

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,22 +1,26 @@
-import React from "react";
-
+import React, {useContext} from "react";
+import { UserContext } from "../context/user";
+import { Link } from 'react-router-dom';
 
 
 function Home(){
+
+  const {currentUser} = useContext(UserContext);
+  
+
   return (
     <div id="home">
       <div id="home-top-div"> 
-      <h2>Welcome</h2>
+      <h2>Welcome to PatchedIn</h2>
       </div>
 
- 
-      <div id="home-middle-div">
-        to the
+    <section id="home-middle-section">
+      <div id="middle-logo-div">
       </div>
- 
+    </section>
 
       <div id="home-bottom-div">
-      Home
+      {currentUser?<h5>Welcome, {currentUser.username}! </h5>:<h5>Become a <Link to="/signup">member</Link> or <Link to="/login">login</Link> to join the growing community of people working together.</h5>}<h5>You can browse the ongoing projects, add tasks and contributions to projects, and start building your own projects with others.</h5>
       </div>
     </div>
   )
