ef4aa489ebc7be69f882ba73c32e6112ca7bc3a6
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-23 07:41:43 -0700

    Styles: the navbar links to the right side and puts spaces between them.

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -14,18 +14,22 @@ function Navigation(){
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
-      <Nav.Link as={Link} to="/" className="text-dark mr-3">Home</Nav.Link>
-      <Nav.Link as={Link} to="/about" className="text-dark mr-3">About</Nav.Link>
-      <NavDropdown title="Projects" id="basic-nav-dropdown" className="text-dark mr-3">
-        <NavDropdown.Item as={Link} to="/projects/me">My Projects</NavDropdown.Item>
-        <NavDropdown.Item as={Link} to="/contributions">My Contributions</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item as={Link} to="/projects">All Projects</NavDropdown.Item>
-      </NavDropdown>
-      <Nav className="ml-auto">
-        <Nav.Link as={Link} to="/signup" className="btn btn-primary">Sign up</Nav.Link>
-        <Nav.Link as={Link} to="/login" className="btn btn-outline-primary">Log in</Nav.Link>
-      </Nav>
+
+        <Nav.Link as={Link} to="/" className="text-dark mr-3">Home</Nav.Link>
+        <Nav.Link as={Link} to="/about" className="text-dark mr-3">About</Nav.Link>
+        <NavDropdown title="Projects" id="basic-nav-dropdown" className="text-dark mr-3">
+          <NavDropdown.Item as={Link} to="/projects/me">My Projects</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/contributions">My Contributions</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item as={Link} to="/projects">All Projects</NavDropdown.Item>
+        </NavDropdown>
+
+      
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/signup" className="btn btn-primary">Sign up</Nav.Link>
+          <Nav.Link as={Link} to="/login" className="btn btn-outline-primary">Log in</Nav.Link>
+        </Nav>
+    
     </Nav>
   </Navbar.Collapse>
 </Navbar>
