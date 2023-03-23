c32818439bef5878e7cf74be1ee5a998406390b8
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-03-23 08:20:55 -0700

    Styles: the about page p elements and changes some of the wording.

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -6,10 +6,11 @@ function About(){
       <div><h3>What Is PatchedIn?</h3></div>
       <div className="about-paragraph"><p>PatchedIn aims to bridge the gap between tech education and the job market by providing a platform for bootcamp students, the self-taught, and other non-tech degree graduates to gain the on-the-job experience necessary to secure entry-level positions in the tech industry. Despite the high cost and rating of bootcamps, many graduates find themselves struggling to land their first job due to the industry's preference for candidates with several years of experience. Furthermore, many of these solo grads and self-learners lack the “Teamwork” experience, the github contribution history, the group project portfolio, to start strong in engineering work, amongst project managers, tech cohorts, design teams, and more. </p></div>
       <div className="about-paragraph"><p>Through our self-propelled startup, PatchedIn will offer participants the opportunity to build their resumes, gain practical experience, and bootstrap their careers in a supportive and innovative environment. Our program will focus on hands-on learning, with participants working on real-world projects and collaborating with industry professionals to gain the skills and experience needed to excel in the tech job market.</p></div>
-      <div className="about-paragraph"><p>Ultimately, our goal is to become the supplementary bootcamp experience that bootcamp graduates need to be competitive in the job market. By providing a platform for hands-on learning and practical experience, PatchedIn will enable participants to develop the skills and connections they need to succeed in the tech industry.</p></div>
+      <div className="about-paragraph"><p>Ultimately, our goal is to become the supplementary bootcamp experience that career pivoters need to be competitive in the job market. By providing a platform for hands-on learning and practical experience, PatchedIn will enable participants to develop the skills and connections they need to succeed in the tech industry.</p></div>
       </div>
       <div className="about-second">
-        More info?
+        <h6>Want More Info?</h6>
+        <p><a href="mailto:j.daniel.saltzman@gmail.com">&#128232; Contact us via email</a></p>
       </div>
       
   
