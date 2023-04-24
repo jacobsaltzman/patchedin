f49bdb8b5432e127a178582b0d49b5c439a343d4
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-04-23 23:26:58 -0700

    Moves: the account info into the toggle dropdown.

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -39,7 +39,7 @@ function Navigation({logo2}){
         </Nav>
          :
          <NavDropdown title="Projects" id="basic-nav-dropdown" className="text-dark mr-3">
-          <NavDropdown.Item as={Link} to="/projects/me">My Projects</NavDropdown.Item>
+          <NavDropdown.Item as={Link}to={`/users/${currentUser.id}`}>My Account</NavDropdown.Item>
           <NavDropdown.Item as={Link} to="/contributions">My Contributions</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item as={Link} to="/projects">All Projects</NavDropdown.Item>
@@ -48,7 +48,7 @@ function Navigation({logo2}){
   </Navbar.Collapse>
 
   {!currentUser?<h6 id="no-user">Sign up or Login to continue!</h6>:""}
-  {currentUser ? <div><Link to={`/users/${currentUser.id}`}>Account</Link><div id="user-greeting"><h6>Welcome, {currentUser.username}</h6></div>   <div id="logout-button"><button onClick={handleLogout}>Log Out</button> </div></div>: null}
+  {currentUser ? <div><div id="user-greeting"><h6>Welcome, {currentUser.username}</h6></div>   <div id="logout-button"><button onClick={handleLogout}>Log Out</button> </div></div>: null}
 
 </Navbar>
   )
