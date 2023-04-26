28e3134cfd88c513fc739ac6c84c5c4de2c25a94
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-04-26 14:18:14 -0700

    Adds: a custom serializer to grab the reports from the contributions of each task.

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,7 +1,10 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :difficulty
+  attributes :id, :name, :description, :difficulty, :messages
   belongs_to :project
   has_many :contributions
 
+  def messages
+    object.contributions.map(&:report)
+  end
   
 end
