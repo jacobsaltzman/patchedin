375c82edebd3c5b19d739ebc0ef4109e24a854af
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-04-26 18:47:02 -0700

    Styles: the difficulty and description of the task card.

--- client/src/Components/Tasks/TasksCard.js
+++ client/src/Components/Tasks/TasksCard.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 
+const difficultyMap = {
+  0: { label: 'N/A', color: 'blue' },
+  1: { label: 'Easy', color: 'green' },
+  2: { label: 'Medium', color: 'yellow' },
+  3: { label: 'Hard', color: 'orange' },
+  4: { label: 'Extreme', color: 'red' },
+};
+
 function TasksCard({id, difficulty, description }) {
+
+  const { label, color } = difficultyMap[difficulty];
   
   return (
     <Link to={`/tasks/${id}`} className="task-card" id={id}>
       <div className="task-info">
       <p className='description'>{description}</p>
-      <p className='difficulty'>{difficulty}</p>
-
+      <p className='difficulty' style={{ color }}>{label}</p>
 
       </div>
 
