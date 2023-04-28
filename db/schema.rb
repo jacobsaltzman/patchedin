f37e58f5d9e35b8b0485219a1272d0e2324ad7b4
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-04-28 13:26:54 -0700

    Adds: new migration for subscribers and migrates to schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_191036) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_202346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_191036) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
+  create_table "subscribers", force: :cascade do |t|
+    t.text "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.bigint "project_id", null: false
     t.string "name"
@@ -51,6 +57,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_191036) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
   end
 
   add_foreign_key "contributions", "tasks"
