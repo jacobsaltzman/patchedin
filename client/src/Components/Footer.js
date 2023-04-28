ae698617e210f169d003950acfe9248f8b2eee38
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-04-28 13:35:07 -0700

    Adds: the form and handling for the email subscription functionality.

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,6 +1,36 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
 
 function Footer() {
+
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch('/subscribe.php', {
+      method: 'POST',
+      body: JSON.stringify({ email }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Thanks for subscribing!');
+      } else {
+        console.error('Failed to subscribe:', response.statusText);
+      }
+    })
+    .catch(error => {
+      console.error('Failed to subscribe:', error);
+    });
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
   return (
     <footer className="bg-dark text-light py-3">
       <Container>
@@ -13,6 +43,18 @@ function Footer() {
             <a href="https://github.com/jacobsaltzman" target="_blank" rel="noopener noreferrer">GitHub</a> |{' '}
             <a href="https://twitter.com/JacobSaltzman3" target="_blank" rel="noopener noreferrer">Twitter</a>
           </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <h1>Subscribe to our newsletter!</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input type="email" value={email} onChange={handleEmailChange} />
+          </label>
+          <button type="submit">Subscribe</button>
+        </form>
         </Row>
       </Container>
     </footer>
